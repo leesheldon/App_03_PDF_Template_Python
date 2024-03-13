86df760ffa1cadf53d198ffadefaf73accbda974
Sheldon <lhanco@gmail.com>
2024-03-13 12:25:36 +0700

    Day 24: Add lines which should be 10mm apart from each other.

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+    # Generate master page
     pdf.add_page()
 
     pdf.set_font(family="Times", style="B", size=22)
     # Set text color to gray color
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 22, 200, 22)
+
+    # Add lines which should be 10mm apart from each other
+    for j in range(20, 298, 10):
+        pdf.line(10, j, 200, j)
 
     # Add Footer for master page
     pdf.ln(265)
@@ -21,6 +25,7 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+    # Generate child pages
     for i in range(row["Pages"] - 1):
         pdf.add_page()
 
@@ -30,6 +35,10 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        # Add lines which should be 10mm apart from each other
+        for j in range(20, 298, 10):
+            pdf.line(10, j, 200, j)
 
 pdf.output("output.pdf")
 
